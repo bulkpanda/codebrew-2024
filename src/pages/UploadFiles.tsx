427748719ebd171209e00a05127ff9cfee2cefb6
Yufei Li <yufei_li19972@outlook.com>
2024-04-06 14:19:48 +1100

    enable uploading photo at frontend

--- src/pages/UploadFiles.tsx
+++ src/pages/UploadFiles.tsx
@@ -1,21 +1,65 @@
-import { FunctionComponent, useCallback } from "react";
-import FrameComponent2 from "../components/FrameComponent2";
+import { FunctionComponent, useCallback, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import FrameComponent2 from "../components/FrameComponent2";
 import FrameComponent1 from "../components/FrameComponent1";
 import FrameComponent from "../components/FrameComponent";
 import "./UploadFiles.css";
 
 const UploadFiles: FunctionComponent = () => {
   const navigate = useNavigate();
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const onUploadFoodFilesClick = useCallback(() => {
-    navigate("/6-scanning-result");
-  }, [navigate]);
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  }, []);
+
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      // console.log(file);
+      const formData = new FormData();
+      formData.append('image', file);
+      const url = 'http://127.0.0.1:8080/googleVision';
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          body: formData,
+        });
+
+        if (response.ok) {
+          // uploading file success
+          const data = await response.json();
+
+          console.log('Upload successful', data);
+
+          navigate("/6-scanning-result");
+        } else {
+          console.error('Upload failed', response.statusText);
+        }
+      } catch (error) {
+        console.error('Upload error', error);
+      }
+    }
+
+
+  };
 
   return (
     <div className="uploading-files">
       <FrameComponent2 />
       <section className="uploadbutton">
+        {/* hidden */}
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+          accept="image/*" 
+          // capture 
+        />
         <img
           className="upload-food-files"
           loading="lazy"
